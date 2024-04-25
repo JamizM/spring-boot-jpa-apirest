7e8546b200f7b18294822b97b85ca1441c2f3813
JamizM <pedringuimaraes09@gmail.com>
2024-04-25 18:25:47 -0300

    Profiles E diversos ambientes de producao

--- src/main/java/br/com/jamiz/MinhaConfiguration.java
+++ src/main/java/br/com/jamiz/MinhaConfiguration.java
@@ -1,10 +1,20 @@
 package br.com.jamiz;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 //Caso o nome da classe seja a mesma do "annotation" ira dar conflito e nao ira importar
 
 @Configuration
+@Profile("development") //so ira rodar no ambiente de desenvolvimento
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("Rodando config");
+        };
+    }
 }
