82fcdb0936fe7dd6ea6284751f0aa2303fd36fe7
JamizM <pedringuimaraes09@gmail.com>
2024-04-29 20:17:08 -0300

    Criacao de Modelos das tabelas

---
+++ src/main/java/br/com/jamiz/domain/entity/Cliente.java
@@ -0,0 +1,22 @@
+package br.com.jamiz.domain.entity;
+
+public class Cliente {
+    private Integer id;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
