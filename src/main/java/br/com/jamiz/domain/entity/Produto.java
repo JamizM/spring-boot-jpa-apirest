b2b0aff5f6b1d40afb52e6e960f5afc5cea1ddd3
JamizM <pedringuimaraes09@gmail.com>
2024-05-01 14:05:25 -0300

    Mapeando Entidade Produto no DB

--- src/main/java/br/com/jamiz/domain/entity/Produto.java
+++ src/main/java/br/com/jamiz/domain/entity/Produto.java
@@ -1,10 +1,28 @@
 package br.com.jamiz.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+
+
 import java.math.BigDecimal;
 
+@Entity
+@Table
 public class Produto {
+
+    @Id //identificar o PK da entidade
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id") //nao é necessario colocar pois tem o mesmo nome na tabela, porem colocamos para facilitar entendimento
     private Integer id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
