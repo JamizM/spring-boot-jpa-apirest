a0011d7312b156f1f542613d00ea64ff52291e77
JamizM <pedringuimaraes09@gmail.com>
2024-05-02 20:41:03 -0300

    Requisicao GET com Response Entity

--- src/main/java/br/com/jamiz/rest/controller/ClienteController.java
+++ src/main/java/br/com/jamiz/rest/controller/ClienteController.java
@@ -1,20 +1,32 @@
 package br.com.jamiz.rest.controller;
 
+import br.com.jamiz.domain.entity.Cliente;
+import br.com.jamiz.domain.repository.Clientes;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes") //toda requisicao que for feita para "/api/clientes" caíra neste controller
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET, consumes = { "application/json", "application/xml"}, produces = { "application/json", "application/xml"}) //diz que retorna um método GET
-    @ResponseBody //colocando esta annotation para dizer que o "return" é o corpo da minha resposta
-    public String helloClientes(@PathVariable("nome") String nomeCliente){ //definindo que vai receber variavel via URL
-        return String.format("hello " + nomeCliente);
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
     }
 
-
+    //    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET) //diz que retorna um método GET
+    @ResponseBody //colocando esta annotation para dizer que o "return" é o corpo da minha resposta
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id){ //definindo que vai receber variavel via URL
+        Optional<Cliente> cliente = clientes.findById(id);
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build(); //usa-se o biuld() quando nao há nada parametrizado
+    }
 }
