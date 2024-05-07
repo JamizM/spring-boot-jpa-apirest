347f787b1a70a850a5dfbc60943f7a234606bc91
JamizM <pedringuimaraes09@gmail.com>
2024-05-07 19:53:04 -0300

    Refatorando para RESTful

--- src/main/java/br/com/jamiz/rest/controller/ClienteController.java
+++ src/main/java/br/com/jamiz/rest/controller/ClienteController.java
@@ -5,14 +5,15 @@ import br.com.jamiz.domain.repository.Clientes;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api/clientes") //toda requisicao que for feita para "/api/clientes" caíra neste controller
 public class ClienteController {
 
@@ -23,59 +24,51 @@ public class ClienteController {
     }
 
     //    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET) //diz que retorna um método GET
-    @ResponseBody //colocando esta annotation para dizer que o "return" é o corpo da minha resposta
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id){ //definindo que vai receber variavel via URL
-        Optional<Cliente> cliente = clientes.findById(id);
-        if(cliente.isPresent()){
-            return ResponseEntity.ok(cliente.get());
-        }
-        return ResponseEntity.notFound().build(); //usa-se o biuld() quando nao há nada parametrizado
+    public Cliente getClienteById(@PathVariable Integer id){ //definindo que vai receber variavel via URL
+        return clientes
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
     }
 
     @PostMapping
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Cliente cliente){
-        Cliente clienteSalvo = clientes.save(cliente);
-        return ResponseEntity.ok(clienteSalvo);
+    @ResponseStatus(HttpStatus.CREATED) //código de retorno para cliente - 201
+    public Cliente save(@RequestBody Cliente cliente){
+        return clientes.save(cliente);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Integer id){
-        Optional<Cliente> cliente = clientes.findById(id);
-
-        if(cliente.isPresent()){
-            clientes.delete(cliente.get()); //usado o delete() pois a classe instancia da classe "Clientes"
-            //tem o Jpa que ja vem método juntos com que
-        }
-        return ResponseEntity.noContent().build();
-        //usado o noContent quando nao vamos returnar nada no corpo da requisicao
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
+    public void delete(@PathVariable Integer id){
+        clientes.findById(id)
+                .map(cliente -> {clientes.delete(cliente);
+                return cliente;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
     }
 
     @PutMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente){
-
-        return clientes.findById(id).map(clienteExistente -> {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Cliente cliente){
+        clientes
+                .findById(id)
+                .map(clienteExistente -> {
             cliente.setId(clienteExistente.getId());
             clientes.save(cliente); //atualiza dentro do banco
-            return ResponseEntity.noContent().build();
-        })
-                .orElseGet( () -> ResponseEntity.notFound().build()); // caso seja buscado o cliente dentro do banco
+            return cliente;
+        }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado")); // caso seja buscado o cliente dentro do banco
         //e nao há o cliente, ele devolve um notFound
         //map() se caso existir o resultado, entra dentro do método map
     }
 
     @GetMapping
-    public ResponseEntity find(Cliente filtro){
+    public List<Cliente> find(Cliente filtro){
         ExampleMatcher matcher = ExampleMatcher
                 .matching()
                 .withIgnoreCase()
                 .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
         Example example = Example.of(filtro, matcher);
-        List<Cliente> lista = clientes.findAll(example); //fazendo pesquisa
+        return clientes.findAll(example); //fazendo pesquisa
         //vai pegar as propriedades populadas do objeto "filtro" e
-        return ResponseEntity.ok(lista);
     }
 }
