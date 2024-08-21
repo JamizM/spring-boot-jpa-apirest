d8556f65ee85ea9ee93977154c20e1a96f9d6a44
23.00983-7 <pedringuimaraes09@gmail.com>
2024-08-21 11:10:30 -0300

    criacao de objetos userdetails em memoria

--- src/main/java/br/com/jamiz/config/SecurityConfig.java
+++ src/main/java/br/com/jamiz/config/SecurityConfig.java
@@ -5,9 +5,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.awt.image.MemoryImageSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,19 +28,34 @@ public class SecurityConfig {
     }
 
     //parte de autenticacao
+
+
+
+    //construcao de um objeto User
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("password1")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("adminPass")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 
     //verificacao se o usuario tem acesso a certas roles dentro do projeto
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
     }
 }
+
+//https://www.youtube.com/watch?v=iWwv9r9inns
