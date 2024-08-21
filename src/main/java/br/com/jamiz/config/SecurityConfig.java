ab368746ef59b932529ce46415d15cbe6e1b5a5d
JamizM <pedringuimaraes09@gmail.com>
2024-08-20 21:08:09 -0300

    arquivo de configuracao security

---
+++ src/main/java/br/com/jamiz/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package br.com.jamiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity //
+public class SecurityConfig {
+
+    //parte de autenticacao
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //verificacao se o usuario tem acesso a certas roles dentro do projeto
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
